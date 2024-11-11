41f43e7662c527fd03c80a5084942753432989cf
Ummam Ali (Lappy) <sblue7446@gmail.com>
2024-11-11 14:07:35 +0500

    added the cors and dummy data

--- dummyIdeas.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  {
-    id: 1,
-    text: "lorem ipsum",
-    tag: "Technology",
-    username: "someone",
-    date: "2022-01-02",
-  },
-];
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const config = require("./config");
 const { router: ideaRouter } = require("./resources/idea");
 const { router: usersRouter } = require("./resources/users");
@@ -11,6 +12,7 @@ connectDB();
 const app = express();
 
 // middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
